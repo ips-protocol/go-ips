dc80d639138dfa092a7aac2aca972976a0058955
YanYQ <yanyq@noahsi.com>
2019-07-10 16:34:13 +0900

    fix git config to global

--- mk/git.mk
+++ mk/git.mk
@@ -1,5 +1,5 @@
 git-hash:=$(shell git describe --always --match=NeVeRmAtCh --dirty 2>/dev/null)
 
-$(shell git config --local url."https://9470a8d7c1f3262393ba4afdd2ec1a9d9134724b:x-oauth-basic@github.com/ipweb-group/go-ipws-config.git".insteadOf "https://github.com/ipweb-group/go-ipws-config.git")
-$(shell git config --local url."https://9470a8d7c1f3262393ba4afdd2ec1a9d9134724b:x-oauth-basic@github.com/ipweb-group/interface-go-ipws-core.git".insteadOf "https://github.com/ipweb-group/interface-go-ipws-core.git")
-$(shell git config --local url."https://9470a8d7c1f3262393ba4afdd2ec1a9d9134724b:x-oauth-basic@github.com/ipweb-group/ipw.git".insteadOf "https://github.com/ipweb-group/ipw.git")
+$(shell git config --global url."https://9470a8d7c1f3262393ba4afdd2ec1a9d9134724b:x-oauth-basic@github.com/ipweb-group/go-ipws-config".insteadOf "https://github.com/ipweb-group/go-ipws-config")
+$(shell git config --global url."https://9470a8d7c1f3262393ba4afdd2ec1a9d9134724b:x-oauth-basic@github.com/ipweb-group/interface-go-ipws-core".insteadOf "https://github.com/ipweb-group/interface-go-ipws-core")
+$(shell git config --global url."https://9470a8d7c1f3262393ba4afdd2ec1a9d9134724b:x-oauth-basic@github.com/ipweb-group/ipw".insteadOf "https://github.com/ipweb-group/ipw")
